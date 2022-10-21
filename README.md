090602ede6c33b5d8078ddbb9343f4f17bacd5be
Web-serfer <redibedi2019@gmail.com>
2022-10-21 18:25:38 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to [DennisIvy.com](https://www.dennisivy.com/)
+
+My personal website re-built With [Astro](https://astro.build).
+
+This is a simple static site that I will use for blogging.
+
+<img style="max-width:500px" src="public/images/preview.PNG">
+
---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  markdown: {
+    // Example: Switch to use prism for syntax highlighting in Markdown
+    syntaxHighlight:'shiki',
+  },
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
