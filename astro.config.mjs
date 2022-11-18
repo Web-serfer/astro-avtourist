2878d1c33218ff4a519e64e9ee0403b3008a7deb
Web-serfer <redibedi2019@gmail.com>
2022-11-18 05:05:55 +0500

    Commit 18/11/22

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), vue()],
   markdown: {
     // Example: Switch to use prism for syntax highlighting in Markdown
     syntaxHighlight: 'shiki'
